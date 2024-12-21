8074fa06581647423ccbeb503d1a33dffa1d66e5
DIMON <user357@rambler.ru>
2024-12-21 22:37:52 +0600

    blog

--- src/main/java/ru/tattoo/maxsim/controller/BlogController.java
+++ src/main/java/ru/tattoo/maxsim/controller/BlogController.java
@@ -1,13 +1,23 @@
 package ru.tattoo.maxsim.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import ru.tattoo.maxsim.service.interf.ReviewService;
+
+import java.time.LocalDateTime;
 
 @Controller
 public class BlogController {
+
+    @Autowired
+    private ReviewService reviewService;
+
     @GetMapping ("/blog")
     public  String blog (Model model){
+        model.addAttribute("localDateTime", LocalDateTime.now());
+        model.addAttribute("count", reviewService.getCount());
         return "blog";
     }
 }
