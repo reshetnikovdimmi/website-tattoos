a328841f7cce0a152747df9fdc0bf3f02c2a241f
reddmix <reshetnikov.dim.mi@yandex.ru>
2025-05-25 23:37:17 +0600

    uploadForm

--- src/main/resources/static/js/admin.js
+++ src/main/resources/static/js/admin.js
@@ -52,3 +52,46 @@ function deleteImage(imageId, selectorToRefresh, deletionUrl) {
         }
     });
 }
+function uploadSection(id, event, fragment) {
+    event.preventDefault(); // Отмена стандартной отправки формы
+    const form = event.target// получаем форму через событие
+    var elements = form.querySelectorAll('[contenteditable]');
+    const formData = new FormData(form); // Создаем объект FormData с полями формы
+     // Добавляем id в FormData
+    formData.append('id', id);
+    const formAction = form.action; // Извлекаем адрес из атрибута action
+    // Переключение между режимами редактирования и отображения
+    elements.forEach(function(element) {
+        if (element.isContentEditable) {
+            element.contentEditable = false;
+        } else {
+            element.contentEditable = true;
+        }
+    });
+
+    // Изменение текста кнопки
+    if (event.target.textContent  === 'Изменить') {
+        event.target.textContent  = 'Сохранить';
+    } else {
+              $.ajax({
+                  url: formAction, // Адрес контроллера Spring MVC
+                  dataType: 'html', // Тип ожидаемого ответа — HTML-фрагмент
+                  cache: false,
+                  contentType: false,
+                  processData: false,
+                  data: formData,
+                  type: 'POST',
+                  success: function(data) {
+                      alert('Татуировка успешно сохранена!');
+                      console.log('Татуировка успешно сохранена!');
+                      $(fragment).html(data); // Замена текущего содержимого новым шаблоном
+                      event.target.textContent = 'Изменить';
+                  },
+                  error: function(jqXHR, textStatus, errorThrown) {
+                      console.log('Ошибка при загрузке:', errorThrown);
+                      alert('Произошла ошибка при сохранении татуировки.');
+                  }
+              });
+
+    }
+}
