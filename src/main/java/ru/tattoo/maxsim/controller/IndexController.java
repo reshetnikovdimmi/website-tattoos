74ad463102660ed260484aaa0ddb676eb0d66e92
reddmix <user357@rambler.ru>
2026-01-26 23:57:01 +0600

    Добавлена обработка в контроллере

--- src/main/java/ru/tattoo/maxsim/controller/IndexController.java
+++ src/main/java/ru/tattoo/maxsim/controller/IndexController.java
@@ -1,8 +1,10 @@
 package ru.tattoo.maxsim.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,16 +14,21 @@ import ru.tattoo.maxsim.service.interf.EmailService;
 import ru.tattoo.maxsim.service.interf.HomeService;
 import ru.tattoo.maxsim.service.interf.ReviewService;
 
+@Slf4j
 @Controller
 public class IndexController {
 
-    @Autowired
-    private EmailService emailService;
-    @Autowired
-    private ReviewService reviewService;
-    @Autowired
-    private HomeService homeService;
+    private final EmailService emailService;
+    private final ReviewService reviewService;
+    private final HomeService homeService;
 
+    public IndexController(EmailService emailService,
+                           ReviewService reviewService,
+                           HomeService homeService) {
+        this.emailService = emailService;
+        this.reviewService = reviewService;
+        this.homeService = homeService;
+    }
 
     @GetMapping("/")
     public String home(Model model) {
@@ -30,14 +37,20 @@ public class IndexController {
     }
 
     @PostMapping("/mail")
-    public String sendMail(@ModelAttribute("details") EmailDetails details, Model model) {
+    public String sendMail(@ModelAttribute("details") EmailDetails details, Model model, BindingResult bindingResult) {
 
-        boolean isSuccess = emailService.sendSimpleMail(details);
+        if (bindingResult.hasErrors()){
+            return "Index::map-contact-form";
+        }
 
-        if (isSuccess) {
-            model.addAttribute("status", "Mail Sent Successfully...");
-        } else {
-            model.addAttribute("status", "Error while Sending Mail");
+        try {
+            boolean isSuccess = emailService.sendSimpleMail(details);
+            model.addAttribute("status", isSuccess ?
+                    "Сообщение отправлено" :
+                    "Ошибка при отправке сообщения");
+        } catch (Exception e){
+            model.addAttribute("status", "Ошибка сервера. Попробуйте позже.");
+            log.error("Error sending email: {}", e.getMessage());
         }
 
         model.addAttribute("details", new EmailDetails());
