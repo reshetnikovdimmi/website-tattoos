12564bb4d0a038f6b0d618f218e750dfae930ee3
reddmix <user357@rambler.ru>
2026-02-01 00:09:42 +0600

    решена проблема избыточности вызова добавлено использование @ModelAttribute для общего метода

--- src/main/java/ru/tattoo/maxsim/controller/IndexController.java
+++ src/main/java/ru/tattoo/maxsim/controller/IndexController.java
@@ -1,14 +1,13 @@
 package ru.tattoo.maxsim.controller;
 
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import ru.tattoo.maxsim.model.ContactInfo;
 import ru.tattoo.maxsim.model.EmailDetails;
 import ru.tattoo.maxsim.service.interf.EmailService;
 import ru.tattoo.maxsim.service.interf.HomeService;
@@ -30,14 +29,19 @@ public class IndexController {
         this.homeService = homeService;
     }
 
+    @ModelAttribute
+    public void addCommonAttributes (Model model){
+        model.addAttribute("reviewsLimit" , reviewService.findLimit());
+        model.addAttribute("carousel" , homeService.findAll());
+    }
+
     @GetMapping("/")
-    public String home(Model model) {
-        prepareModel(model);
+    public String home(@ModelAttribute("details") EmailDetails details) {
         return "Index";
     }
 
     @PostMapping("/mail")
-    public String sendMail(@ModelAttribute("details") EmailDetails details, Model model, BindingResult bindingResult) {
+    public String sendMail(@Valid @ModelAttribute("details") EmailDetails details, Model model, BindingResult bindingResult) {
 
         if (bindingResult.hasErrors()){
             return "Index::map-contact-form";
@@ -55,11 +59,5 @@ public class IndexController {
 
         model.addAttribute("details", new EmailDetails());
         return "Index::map-contact-form";
-    }
-
-    private void prepareModel(Model model) {
-        model.addAttribute("reviewsLimit", reviewService.findLimit());
-        model.addAttribute("details", new EmailDetails());
-        model.addAttribute("carousel", homeService.findAll());
     }
 }
